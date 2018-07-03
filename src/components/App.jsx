0048532edb6af0cadaca346531db4bb5b528d155
Jared Lutz and Matt Nardoni <matt@>
2018-07-03 16:36:23 -0700

    set up components

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+import Tamagotchi from './Tamagotchi';
+
+
+function App() {
+
+  return (
+    <div>
+
+      <Switch>
+        <Route exact path='/' component={Tamagotchi}/>
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/Tamagotchi.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tam from '../assets/images/tam.png';
+
+function Tamagotchi(){
+  return(
+    <div>
+      <img src={Tam} alt='Tamagotchi' />
+    </div>
+  )
+}
+
+export default Tamagotchi;
